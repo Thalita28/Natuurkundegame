86027af23d248bb1a1a130692aaeaef19b6d4636
Aaron van Diepen <a.diep95@gmail.com>
2019-12-09 11:48:07 +0100

    moet

---
+++ Assets/Scripts/QuitGameOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class QuitGameOnClick : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Quit"); // debug omdat in de unity game engine het spel niet sluit op deze manier
+        Application.Quit(); // sluit spel af
+    }
+}
