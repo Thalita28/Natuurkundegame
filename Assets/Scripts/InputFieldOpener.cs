4e3286888a3edd4bd125d692522994d771576529
Aaron van Diepen <a.diep95@gmail.com>
2020-01-02 11:22:01 +0100

    cloned van master

---
+++ Assets/Scripts/InputFieldOpener.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputFieldOpener : MonoBehaviour
+{
+    public GameObject InputPanel;
+  
+
+    void ActivatePanel()
+    {
+        InputPanel.SetActive(true);
+    }
+
+ 
+}
---
+++ Assets/Scripts/SetPlayerName.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SetPlayerName : MonoBehaviour
+{
+    public TextMeshProUGUI playername;
+    private TextMeshProUGUI myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = gameObject.GetComponent<TextMeshProUGUI>();
+        myText.text = PlayerPrefs.GetString("PlayerName", "Player Name");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SubmitName()
+    {
+        PlayerPrefs.SetString("PlayerName", playername.text);
+        //myText.text = playername.text; //alleen voor testen in editor
+        myText.text = PlayerPrefs.GetString("PlayerName", "Player Name");
+        //Debug.Log(playername.text);
+    }
+}
