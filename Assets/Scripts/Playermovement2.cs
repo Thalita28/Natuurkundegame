3f9dd944dbbf2e309a62ad2f42e82816fd095544
LeviArts <49712323+LeviArts@users.noreply.github.com>
2020-01-05 14:47:04 +0100

    fuel fix

--- Assets/Scripts/Playermovement2.cs
+++ Assets/Scripts/Playermovement2.cs
@@ -7,6 +7,7 @@ public class Playermovement2 : MonoBehaviour
 {
     private Rigidbody rb;
     public TextMeshProUGUI MotionFeedback;
+    public Transform FuelBar;
 
     private bool checkingForStop;
     private float stopTimer;
@@ -16,6 +17,7 @@ public class Playermovement2 : MonoBehaviour
     public float RotateSpeed;
     public float FuelUsed = 0.0f;
     public float MaxSpeed = 20.0f;
+    private int StartingFuel;
 
     void Start()
     {
@@ -23,22 +25,24 @@ public class Playermovement2 : MonoBehaviour
         checkingForStop = false;
         FuelUsed = 0;
         rb = GetComponent<Rigidbody>();
+        StartingFuel = PlayerPrefs.GetInt("StartingFuel", 10000);
     }
 
     public void Update()
     {
-        if(IsMoving) Movement();
+        if (IsMoving) Movement();
+        UpdateFuelBar();
         CheckForCompleteStop();
         var move_vec = rb.velocity;
         MotionFeedback.text = "Speed: " + move_vec.magnitude + "\nVector: " + move_vec + "\nFuel used: " + FuelUsed;
 
-        
+
 
     }
 
     private void CheckForCompleteStop()
     {
-        if(rb.velocity.magnitude < 0.5 && rb.velocity.magnitude != 0 && !checkingForStop)
+        if (rb.velocity.magnitude < 0.5 && rb.velocity.magnitude != 0 && !checkingForStop)
         {
             stopTimer = Time.time;
             checkingForStop = true;
@@ -74,11 +78,13 @@ public class Playermovement2 : MonoBehaviour
             ZAxisMovement = 0;
         }
 
-
-        rb.AddForce(Vector3.forward * ZAxisMovement * ThrusterPower, ForceMode.Impulse);
-        FuelUsed += Mathf.Abs((int)(ZAxisMovement * ThrusterPower));
-        rb.AddForce(Vector3.right * XAxisMovement * ThrusterPower, ForceMode.Impulse);
-        FuelUsed += Mathf.Abs((int)(XAxisMovement * ThrusterPower));
+        if (StartingFuel > FuelUsed)
+        {
+            rb.AddForce(Vector3.forward * ZAxisMovement  *ThrusterPower, ForceMode.Impulse);
+            FuelUsed += Mathf.Abs((int)(ZAxisMovement * ThrusterPower));
+            rb.AddForce(Vector3.right * XAxisMovement * ThrusterPower, ForceMode.Impulse);
+            FuelUsed += Mathf.Abs((int)(XAxisMovement * ThrusterPower));
+        }
 
         if (RotateControls == true)
         {
@@ -104,5 +110,13 @@ public class Playermovement2 : MonoBehaviour
         IsMoving = false;
     }
 
-
+    public void UpdateFuelBar()
+    {
+        var FuelBarLeft = (StartingFuel - FuelUsed) / StartingFuel;
+        if (FuelBarLeft < 0)
+        {
+            FuelBarLeft = 0;
+        }
+        FuelBar.transform.localScale = new Vector3(1, FuelBarLeft, 1);
+    }
 }
