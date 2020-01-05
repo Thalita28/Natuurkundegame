d625568e29e7dea0f27b9d2d0d3d3f4efb573285
LeviArts <49712323+LeviArts@users.noreply.github.com>
2020-01-05 14:32:18 +0100

    niks

--- Assets/Scripts/Playermovement2.cs
+++ Assets/Scripts/Playermovement2.cs
@@ -7,7 +7,6 @@ public class Playermovement2 : MonoBehaviour
 {
     private Rigidbody rb;
     public TextMeshProUGUI MotionFeedback;
-    public Transform FuelBar;
 
     private bool checkingForStop;
     private float stopTimer;
@@ -17,7 +16,6 @@ public class Playermovement2 : MonoBehaviour
     public float RotateSpeed;
     public float FuelUsed = 0.0f;
     public float MaxSpeed = 20.0f;
-    private int StartingFuel;
 
     void Start()
     {
@@ -25,17 +23,15 @@ public class Playermovement2 : MonoBehaviour
         checkingForStop = false;
         FuelUsed = 0;
         rb = GetComponent<Rigidbody>();
-        StartingFuel = PlayerPrefs.GetInt("StartingFuel", 10000);
     }
 
     public void Update()
     {
         if(IsMoving) Movement();
-        UpdateFuelBar();
         CheckForCompleteStop();
         var move_vec = rb.velocity;
         MotionFeedback.text = "Speed: " + move_vec.magnitude + "\nVector: " + move_vec + "\nFuel used: " + FuelUsed;
-        
+
         
 
     }
@@ -78,13 +74,11 @@ public class Playermovement2 : MonoBehaviour
             ZAxisMovement = 0;
         }
 
-        if (StartingFuel > FuelUsed)
-        {
-            rb.AddForce(Vector3.forward * ZAxisMovement * ThrusterPower, ForceMode.Impulse);
-            FuelUsed += Mathf.Abs((int)(ZAxisMovement * ThrusterPower));
-            rb.AddForce(Vector3.right * XAxisMovement * ThrusterPower, ForceMode.Impulse);
-            FuelUsed += Mathf.Abs((int)(XAxisMovement * ThrusterPower));
-        }
+
+        rb.AddForce(Vector3.forward * ZAxisMovement * ThrusterPower, ForceMode.Impulse);
+        FuelUsed += Mathf.Abs((int)(ZAxisMovement * ThrusterPower));
+        rb.AddForce(Vector3.right * XAxisMovement * ThrusterPower, ForceMode.Impulse);
+        FuelUsed += Mathf.Abs((int)(XAxisMovement * ThrusterPower));
 
         if (RotateControls == true)
         {
@@ -110,13 +104,5 @@ public class Playermovement2 : MonoBehaviour
         IsMoving = false;
     }
 
-    public void UpdateFuelBar()
-    {
-        var FuelBarLeft = (StartingFuel - FuelUsed) / StartingFuel;
-        if(FuelBarLeft < 0)
-        {
-            FuelBarLeft = 0;
-        }
-        FuelBar.transform.localScale = new Vector3(1, FuelBarLeft, 1);
-    }
+
 }
